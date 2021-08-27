3c2e8c4d56c0d92f968745f5530c187981442caa
areyoureadthemanual <areyoureadthemanual@gmail.com>
2021-08-27 18:10:03 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('libraries', default = './libraries', nargs='?', help='Path to libraries logs.\nExample: ./libraries')
+argparser.add_argument('libraries', default='./libraries', nargs='?', help='Path to libraries logs.\nExample: ./libraries')
 argparser.add_argument('--platform', default='all', help='can be one of [all, linux, macosx, windows]')
 args = argparser.parse_args()
 
@@ -25,12 +25,11 @@ def parser(libraries_path, platform='all'):
 
     for platform in platforms:
         path = Path(libraries_path, platform)
-
         txt_files = [i for i in path.glob('**/*.txt')]
         end_files = [i.stem for i in path.glob('**/*.end')]
 
         for txt_file in txt_files:
-            file_dict = {'OS': platform.capitalize(), 'Architecture': txt_file.parts[3], 'Domain': txt_file.parts[4],}
+            file_dict = {'OS': platform.capitalize(), 'Architecture': txt_file.parts[3], 'Domain': txt_file.parts[4], }
             if txt_file.stem in end_files:
                 file_dict['Optimization'] = txt_file.stem[-2:]
 
@@ -45,6 +44,7 @@ def parser(libraries_path, platform='all'):
                         elif ".Successes" in line:
                             line = line.strip('\n| ')
                             number_of_successes = line.lstrip('.Successes : ')
+
                     if all([number_of_tests, number_of_successes]):
                         success_percent = 100 * float(number_of_successes)/float(number_of_tests)
                         success_percent = format(success_percent, '.2f')
